6e330a03c12f7e592f751fcaa3b9694687c730f0
KjennethObrice <kenneth.olibrice@gmail.com>
2023-02-26 03:17:39 -0500

    Updated peripheral read and peripheral data transmission format.
    Check issues for more information.

--- ArduinoSerialCommunicationCode/ArduinoSerialCommunicationCode.ino
+++ ArduinoSerialCommunicationCode/ArduinoSerialCommunicationCode.ino
@@ -1,12 +1,11 @@
 #include <Wire.h>
 #define SENSOR_ADDRESS 0x52
 
-uint8_t* dataBuffer = new uint8_t[12]; // RGB
+uint8_t* dataBuffer = new uint8_t[14]; // Proximity Sensor, Infared, Green, Blue, Red (In order of reception).
 
 void setup() {
   Serial.begin(9600);
   Wire.begin();
-  
   writeRegister(0x0, 0b0110);
 
   // Clear trash data to prevent undefined behavior.
@@ -16,15 +15,27 @@ void setup() {
 
 }
 
+/** RIO-ARDUINO PROTOCOL:
+  1. RoboRIO will read information from available peripheral devices.
+  2. Arduino will send any available peripheral data to the RoboRIO.
+  3 When the Driver/Operator wants to signal a color, 
+       they will press a button and the RoboRIO will run a command to periodically ping the Arduino with required information. 
+       The arduino will acknowledge and the RoboRIO will stop pinging.
+  4. The Arduino will adjust the LEDs accordingly.
+*/
 void loop() {
   getPeripheralData(dataBuffer);
   if(Serial.availableForWrite() >= 12) {
     sendDataToRoboRIO();
   }
 
+  if(Serial.available() > 0) { // RoboRIO has something to say.
+    interperetCommand(Serial.read());
+  }
   delay(150);
-  
 }
+
+
 
 void readRegisters(uint8_t reg, int numBytes, uint8_t* buff) {
   Wire.beginTransmission(SENSOR_ADDRESS); 
@@ -46,15 +57,23 @@ void writeRegister(uint8_t reg, uint8_t value) {
 }
 
 void getPeripheralData(uint8_t* outArray) {
-  readRegisters(0xA, 12, outArray);
+  readRegisters(0x08, 14, outArray);
 }
 
 /**
-  The RoboRIO will receive each rgb value individually delimited by \n characters.
+  Index values:
+  [0-1]: Proximity sensor.
+  [2-4]: Infared.
+  [5-7]: Green sensor data.
+  [8-10]: Blue sensor data.
+  [11-13]: Red sensor data.
 */
 void sendDataToRoboRIO() {
-  for(int i = 0; i < 12; i++) {
+  for(int i = 0; i < 14; i++) {
     Serial.write(dataBuffer[i]);
-    
   }
+}
+
+void interperetCommand(uint8_t command) {
+  // TODO Implement this function.
 }
