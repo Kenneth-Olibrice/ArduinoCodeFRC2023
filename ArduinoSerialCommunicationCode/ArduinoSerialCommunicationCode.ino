78eb6b2b0cec61d82fe6adcea84819811fd62a64
Kenneth Olibrice <100235157+Kenneth-Olibrice@users.noreply.github.com>
2023-02-28 20:00:06 -0500

    t.

--- ArduinoSerialCommunicationCode/ArduinoSerialCommunicationCode.ino
+++ ArduinoSerialCommunicationCode/ArduinoSerialCommunicationCode.ino
@@ -4,30 +4,23 @@
 #define SENSOR_ADDRESS 0x52
 #define NEOPIXEL_PIN 8
 #define PS_DATA_0 0x08 // First register in the data block read. 
+#define PROXIMITY_THRESHOLD 20
 
 
-uint8_t* dataBuffer = new uint8_t[14]; // Proximity Sensor, Infared, Green, Blue, Red (In order of reception).
-uint8_t* LED_ADDRESSES = new uint8_t[2];
-uint8_t* rgbValuesToSend = new uint8_t[3];
+uint8_t* valuesFromSensor = new uint8_t[14]; // Proximity Sensor, Infared, Green, Blue, Red (In order of reception).
+uint8_t* proximityRGB = new uint8_t[4];
+uint16_t hue = 0;
 Adafruit_NeoPixel communicationStrip(30, NEOPIXEL_PIN, NEO_GRB); // This is the strip the human player will use to determine the desired gamepiece.
 
 void setup() {
   Serial.begin(9600);
   Wire.begin();
+  communicationStrip.begin();
   writeRegister(0x0, 0b0111); // Set the read modes for the color sensor.
   writeRegister(0x04,0b01000000); // Set the color sensor resolution and measurement speed. TODO tune this value and see its effect.
-
-  // Clear trash data to prevent undefined behavior.
-  for(int i = 0; i < 14; i++) {
-    dataBuffer[i] = 0; 
+  for(int i = 0; i < 14; i++) { // Clear trash data to prevent undefined behavior.
+    valuesFromSensor[i] = 0; 
   }
-
-  // TODO find the device addresses.
-  LED_ADDRESSES[0] = 0; // This is the LED strip that indicates the currently held gamepiece.
-  LED_ADDRESSES[1] = 0; // This is the LED strip that allows the driver/operator to signal to the human player which gamepiece they want placed onto the field.
-  
-  communicationStrip.begin();
-
 }
 
 /** RIO-ARDUINO PROTOCOL:
@@ -39,24 +32,31 @@ void setup() {
   4. The Arduino will adjust the LEDs accordingly.
 */
 void loop() {
-  getPeripheralData(dataBuffer);
-  interperetPeripheralData(dataBuffer, rgbValuesToSend);
-
-
-
+  getPeripheralData(valuesFromSensor);
+  pullProximityAndRGBValues(valuesFromSensor, proximityRGB);
   if(Serial.available() > 0) { // RoboRIO has something to say.
     // TODO add the read function once its finished to this section.
   }
 
-
-  uint32_t packedRGB = communicationStrip.ColorHSV(getHue(rgbValuesToSend),255, 255); // Full hue, full brightness
-  // uint32_t packedRGB = communicationStrip.ColorHSV(getHue(rgbValuesToSend),255, 255); // Full hue, half brightness.
-  // uint32_T packedRGB =  communicationStrip.ColorHSV(getHue(rgbValuesToSend),255, 0); // Full hue, 0 brightness (lights should be off)
-  for(int i =0; i < 30; i++) {
-    communicationStrip.setPixelColor(i, packedRGB);
-    // communicationStrip.setPixelColor(i, rgbValuesToSend[0], rgbValuessToSend[1], rgbValuesToSend[2]); // Send RGB Values
-    
+  hue = getHue(proximityRGB);
+  uint8_t* trueRGB[3];
+  if(proximityRGB[3] > PROXIMITY_THRESHOLD) {
+  if(hue > 32000 || hue < 3000) {
+  trueRGB[0] = 148;
+  trueRGB[1] = 0;
+  trueRGB[2] = 211;
+  } else {
+    trueRGB[0] = 255;
+    trueRGB[1] = 255;
+    trueRGB[2] = 0; 
   }
+  for(int i =0; i < 30; i++) {
+    communicationStrip.setPixelColor(i, trueRGB[0], trueRGB[1], trueRGB[2]);
+    // communicationStrip.setPixelColor(i, proximityRGB[0], rgbValuessToSend[1], proximityRGB[2]); // Send RGB Values
+  }
+  }
+  Serial.print("proximity:");
+  Serial.println(proximityRGB[3]);
   communicationStrip.show();
   delay(150);
 }
@@ -86,13 +86,14 @@ void getPeripheralData(uint8_t* outArray) {
   readRegisters(PS_DATA_0, 14, outArray);
 }
 
-void interperetPeripheralData(uint8_t* inArray, uint8_t* outArray) {
+void pullProximityAndRGBValues(uint8_t* inArray, uint8_t* outArray) {
   uint16_t red = (inArray[11])|((inArray[12]<<8)&0xff);
   uint16_t green = (inArray[5])|((inArray[6]<<8)&0xff);
   uint16_t blue = (inArray[8])|((inArray[9]<<8)&0xff);
   outArray[0] = inArray[11];
   outArray[1] = inArray[5];
   outArray[2] = inArray[8];
+  outArray[3] = inArray[0];
 }
 
 /**
@@ -104,8 +105,8 @@ void interperetPeripheralData(uint8_t* inArray, uint8_t* outArray) {
   [11-13]: Red sensor data.
 */
 void sendDataToRoboRIO() {
-  for(int i = 0; i < 14; i++) {
-    Serial.write(dataBuffer[i]);
+  for(int i = 0; i < 4; i++) {
+    Serial.write(proximityRGB[i]);
   }
 }
 
@@ -120,11 +121,8 @@ uint16_t getHue(uint8_t* rgbArray) {
   for(int i = 0; i < 3; i++) {
     if(rgbArray[i] != maxValue && rgbArray[i] != minValue) intermediateValue = rgbArray[i];
   }
-
-  // double hue = (uint16_t) ((intermediateValue - minValue) / maxValue)*360; // Express this ratio in degrees.
-  double hue = (uint16_t) ((intermediateValue - minValue) / maxValue)*65535; // Express this ratio in 16bits.
+  uint16_t hue = (uint16_t) (((intermediateValue - minValue) / maxValue)*65535); // Express this ratio in 16bits.
   Serial.print("Hue: ");
   Serial.println(hue);
   return hue;
-  
 }
